f6f65297f31960d41e59093a505d717eaef7129e
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2022-08-08 12:28:33 +0300

    Burger menu is ready

--- src/assets/js/intro.js
+++ src/assets/js/intro.js
@@ -1,29 +1,39 @@
-const maxWindowWidth = 1920;
 const intro = document.querySelector('.intro')
-const introMainText = document.querySelector('.intro__main-text');
-const introNftItems = document.querySelectorAll('.nft');
-const introInner = document.querySelector('.intro__inner');
+let isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
 
-//change scale of intro main text
 
-// window.addEventListener('resize', resizeIntro);
-// window.addEventListener('load', resizeIntro);
-
-// function resizeIntro() {
-//     if (window.innerWidth < maxWindowWidth) {
-//         introMainText.style.transform = `scale(${window.innerWidth/maxWindowWidth})`;
-
-//         introNftItems.forEach (item => {
-//             item.style.transform = `scale(${window.innerWidth/maxWindowWidth})`;
-//         })
-//     }
-// }
-
-
-// else {
-//     introMainText.style.transform = 'scale(1)';
-
-//     introNftItems.forEach (item => {
-//         item.style.transform = 'scale(1)';
-//     })
-// }
+//action when website is opening on mobile devices
+if(isMobile.any()) {
+    document.body.classList.add('_touch');
+    window.addEventListener('resize', () => {
+        intro.style.height = `${document.documentElement.clientHeight}px`;
+    });
+    intro.style.height = `${document.documentElement.clientHeight}px`;
+} else {
+    document.body.classList.add('_pc');
+};
--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,7 +1,14 @@
 const header = document.querySelector('header');
-const burgerMenu = document.querySelector('.burger__container');
-const navMenu = document.querySelector('.nav');
+const burgerMenu = document.querySelector('.burger');
+const navMenu = document.querySelector('.top-bar__nav');
+
 let scrollPos;
+
+
+//deactivate viewport size changes when keyboard pop up on mobile devices;
+if ("virtualKeyboard" in navigator) {
+    navigator.virtualKeyboard.overlaysContent = true;
+};
 
 
 //show fixed header
@@ -26,3 +33,20 @@ if (burgerMenu) {
         navMenu.classList.toggle('_active');
     })
 };
+
+/* add fixed height value for activation overflow scrolling,
+when a mobile device in horizontal pos */
+window.addEventListener('load', changeHeight, false);
+window.addEventListener('resize', changeHeight, false);
+function changeHeight() {
+    if (window.outerHeight < 450) {
+        navMenu.style.height = `${window.outerHeight}px`;
+        navMenu.style.overflow = 'auto';
+
+    } else {
+        navMenu.style.height = 'auto';
+        navMenu.style.overflow = 'hidden';
+    }
+};
+
+
